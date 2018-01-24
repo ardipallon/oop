f0ac736f0a1fb4cd6170a874f2027684169c8161
ardip <ardi.pallon@khk.ee>
2018-01-24 09:34:42 +0200

    Konfiguratsioon on täiendatud lingi loomise objektiga

--- model/linkobjekt.php
+++ model/linkobjekt.php
@@ -6,7 +6,20 @@
  * Time: 09:16
  */
 
-class linkobjekt
+class linkobjekt extends http
 {
+    // klassi muutujad/väljad
+    var $baseLink = false; // põhilink
+    var $protocol = 'http://'; // HTTP protokoll
+    var $eq = '='; // =
+    var $delim = '&amp;'; // &
 
+    /**
+     * linkobjekt constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(); // kõigepealt defineerime vajalikud eelandmed
+        $this->baseLink = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }
 }
