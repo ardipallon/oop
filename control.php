948f730bcaebb5c8fba6bbe3de97a59355c95b31
ardip <ardi.pallon@khk.ee>
2018-01-31 11:04:21 +0200

    Valitud kontrolleri töö

--- control.php
+++ control.php
@@ -9,4 +9,9 @@ $control = $http->get('control'); // Kontrolleri faili nimi
 $file = CONTROL_DIR.$control.'php'; // Kontrolleri faili tee
 if(file_exists($file) and is_file($file) and is_readable($file)){
     require_once $file;
+} else {
+    // kui ei ole kontrolleri faili või
+    // ei ole veel midagi valitud
+    $file = CONTROL_DIR.DEFAULT_CONTROL.'.php';
+    require_once $file;
 }
