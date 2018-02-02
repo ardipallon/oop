43fbc6de84df75519fae61f47608ce1ab1357a03
ardip <ardi.pallon@khk.ee>
2018-02-02 09:13:18 +0200

    Paika pandud andmebaasiga ühendus

--- model/mysql.php
+++ model/mysql.php
@@ -8,5 +8,31 @@
 
 class mysql
 {
+    // klassi väljad
+    var $conn = false; // Ühendus db serveriga
+    var $host = false; // db server
+    var $user = false; // db server user
+    var $pass = false; // db server kasutaja parool
+    var $dbname = false;
 
+    /**
+     * mysql constructor.
+     * @param bool $conn
+     */
+    public function __construct($host, $user, $pass, $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->connect();
+    }
+
+    function connect($host, $user, $pass, $dbname){
+        $this->conn = mysqli_connect($host, $user, $pass, $dbname);
+        if(!$this->conn){
+            echo 'Probleem andmebaasi ühendusega<br />';
+            exit;
+        }
+    }
 }
