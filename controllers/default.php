595c0aa83c68392f6c15c51e90b36149df8090a8
ardip <ardi.pallon@khk.ee>
2018-02-06 13:02:19 +0200

    Koostati päring contenti jaoks

--- controllers/default.php
+++ controllers/default.php
@@ -5,4 +5,16 @@
  * Date: 31.01.2018
  * Time: 11:00
  */
-$mainTmpl->set('content', 'Vaikimisi sisu');
+$page_id = (int)$http->get('page_id'); // Lehe identifikaator
+// koostame päringu contenti jaoks
+$sql = 'SELECT * from content WHERE content_id ='.fixDB($page_id);
+// küsime andmed andmebaasist
+$result = $db->getData($sql);
+if(result == false){
+    $sql = 'SELECT * FROM content WHERE is_first_page='.fixDB(1);
+    $result = $db->getData($sql);
+}
+if($result != false){
+    $page = $result[0];
+    $mainTmpl->set('content', 'content');
+}
--- menu.php
+++ menu.php
@@ -13,34 +13,6 @@ $itemTmpl = new template('menu.item'); // menüü elemendi mall
 $sql = 'SELECT content_id, content, title FROM content WHERE parent_id='.fixDB(0).' AND show_in_menu='.fixDB(1);
 $result = $db->getData($sql);
 
-// loome üks menüü element nimega esimene
-$itemTmpl->set('name', 'esimene');
-// määrata menüüs väljastava elemendiga seotud link
-// http://ap.ikt.khk.ee/oop/index.php?control=esimene
-$link = $http->getLink(array('control'=>'esimene'));
-$itemTmpl->set('link', $link);
-// lisame antud element menüüsse
-$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
-$menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
-// loome veel üks menüü element nimega teine
-$itemTmpl->set('name', 'teine');
-// määrata menüüs väljastava elemendiga seotud link
-// http://ap.ikt.khk.ee/oop/index.php?control=esimene
-$link = $http->getLink(array('control'=>'teine'));
-$itemTmpl->set('link', $link);
-// lisame antud element menüüsse
-$menuItem = $itemTmpl->parse(); // string, mis sisaldab ühe nimekirja elemendi lingiga
-$menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>
-$itemTmpl->set('name', 'avaleht');
-$link = $http->getLink(array('control'=>'default'));
-$itemTmpl->set('link', $link);
-$menuTmpl->add('menu_items', $itemTmpl->parse());
-
-// ehitame valmis menüü
-$menu = $menuTmpl->parse();
-// lisame valmis menüü lehele
-$mainTmpl->set('menu', $menu);
-
 // kui andmed on andmebaasis olemas, siis loome menüü nende põhjal
 if($result != false){
     foreach($result as $page){
@@ -51,4 +23,4 @@ if($result != false){
     }
 }
 // Paneme paika valmismenüü
-$mainTmpl->set('menu', $mainTmpl->parse());
+$mainTmpl->set('menu', $menuTmpl->parse());
